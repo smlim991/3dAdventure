f124a44c4199f0558f5241d6a33a865b89c891b7
smlim991 <47668659+smlim991@users.noreply.github.com>
2021-11-04 18:28:48 -0400

    Testing Button to trigger invisibility

---
+++ Assets/Code/GlobalVar.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class GlobalVar{
+    public static bool Invisible = true;
+}
---
+++ Assets/Code/HintButton.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player") ){
+            if(GlobalVar.Invisible == true){
+                GlobalVar.Invisible = false;
+            }
+            else{
+                GlobalVar.Invisible = true;
+
+            }
+        }
+    }
+}
