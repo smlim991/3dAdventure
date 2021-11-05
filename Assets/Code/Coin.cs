f9d60d6aad742a833810a597199c710f55535ba7
smlim991 <47668659+smlim991@users.noreply.github.com>
2021-11-04 22:42:58 -0400

    Added Score and counting system

--- Assets/Code/Coin.cs
+++ Assets/Code/Coin.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Coin : MonoBehaviour
 {
     int speed = 100;
-    
+    public Text scoreOut;
 
     
     void Update()
@@ -18,6 +19,7 @@ public class Coin : MonoBehaviour
         
         if(other.CompareTag("Player")){
             GlobalVar.Score ++;
+            scoreOut.text = GlobalVar.Score.ToString();
             Destroy(gameObject);
             
         }
