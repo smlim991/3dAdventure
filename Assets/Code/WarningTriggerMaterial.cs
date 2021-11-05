1e5939758d136f324644de59c6712cf9335708b1
smlim991 <47668659+smlim991@users.noreply.github.com>
2021-11-04 22:58:15 -0400

    Breakable and traps have alternating material when button is pressed

---
+++ Assets/Code/WarningTriggerMaterial.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningTriggerMaterial : MonoBehaviour
+{
+    public Material warningMat;
+    Material defaultMat;
+    Renderer rend;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        defaultMat = rend.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GlobalVar.Invisible == false){
+            rend.material = defaultMat;
+        }
+        else{
+            rend.material = warningMat;
+        }
+    }
+}
