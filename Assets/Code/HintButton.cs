72e7ac47d7a4e1e59694f261b82a2755c66978ea
Sicong Liu <sicong.liu.simon@outlook.com>
2021-11-12 14:18:30 -0500

    Add new button model

--- Assets/Code/HintButton.cs
+++ Assets/Code/HintButton.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class HintButton : MonoBehaviour
 {
     bool enabled;
+    Vector3 scale;
     // Start is called before the first frame update
 
     void Start()
     {
-        
+        scale = transform.localScale;
     }
 
     // Update is called once per frame
@@ -23,6 +24,7 @@ public class HintButton : MonoBehaviour
             if(!GlobalVar.Invisible && GlobalVar.numKey>0)
             {
                 GlobalVar.Invisible = true;
+                transform.localScale = new Vector3(transform.localScale.x, scale.y/5, transform.localScale.z);
             }
         }
     }
@@ -32,6 +34,7 @@ public class HintButton : MonoBehaviour
             if(GlobalVar.Invisible)
             {
                 GlobalVar.Invisible = false;
+                transform.localScale = scale;
                 if(GlobalVar.numKey>0) GlobalVar.numKey-=1;
             }
         }
