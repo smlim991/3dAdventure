460f536ed15184a1a48e7b615c9bef8ed6b24f04
Sicong Liu <sicong.liu.simon@outlook.com>
2021-11-11 20:31:43 -0500

    Add nextlevel plane

--- Assets/Code/NextLevel.cs
+++ Assets/Code/NextLevel.cs
@@ -6,16 +6,21 @@ using UnityEngine.SceneManagement;
 public class NextLevel : MonoBehaviour
 {
     public int limit;
+    float xoffset,zoffset;
+
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player =  GameObject.Find("Player");
+        xoffset = transform.position.x - player.transform.position.x;
+        zoffset = transform.position.z - player.transform.position.z;
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = new Vector3(player.transform.position.x + xoffset, transform.position.y, player.transform.position.z + zoffset);
     }
 
     private void OnTriggerEnter(Collider other) {
